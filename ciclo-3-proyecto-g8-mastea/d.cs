16d75e42d7dd4e75d1857d9698508d708361aa88
IvanMcNish <cesar_mcnish@hotmail.com>
2022-09-09 17:50:01 -0500

    Modificacion de el nombre del proyecto para bloq bin y obj

---
+++ ciclo-3-proyecto-g8-mastea/d.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Proyecto-Cyclo-3.Taller.op
+{
+    public class d
+    {
+     sa   
+    }
+}
