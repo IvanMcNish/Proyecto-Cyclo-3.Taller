e0b846c4d54899217ba38bd62d92f8b9afca7b3c
IvanMcNish <cesar_mcnish@hotmail.com>
2022-09-12 22:26:03 -0500

    Actualizacion Git ignore

---
+++ Taller2.app/Taller.App.Front/Pages/Usuario/IniciarSesion/IniciarSesion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Taller.app.Front.Pages
+{
+    public class IniciarSesionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Taller2.app/Taller.App.Persistencia/AppRepositorios/ContextDb.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Taller.App.Dominio.Mecanico;
+using Microsoft.EntityFrameworkCore;
+
+namespace Taller.App.Persistencia.AppRepositorios
+{
+    public class ContextDb :DbContext
+    {
+        public virtual DbSet<Mecanico> Mecanicos {get;set;}
+         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            try{
+                optionsBuilder.UseSqlServer("Server=tcp:server-tallertic-4.database.windows.net,1433;Initial Catalog=bd_tallertic;Persist Security Info=False;User ID=admintic;Password=Tallermintic4;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                }
+            catch{
+                
+                }
+        }
+    }
+}
