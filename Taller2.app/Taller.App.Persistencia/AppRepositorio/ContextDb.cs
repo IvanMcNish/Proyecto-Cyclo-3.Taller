b34535394399794af5c61d2d01f6ed0149854988
ivanmcnish <cesar_mcnish@hotmail.com>
2022-09-13 14:49:11 -0500

    Conexion con base de datos, y actualizacion de codigo

--- Taller2.app/Taller.App.Persistencia/AppRepositorio/ContextDb.cs
+++ Taller2.app/Taller.App.Persistencia/AppRepositorio/ContextDb.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Taller.App.Dominio.Mecanico;
+using Taller.App.Dominio.Entidades;
 using Microsoft.EntityFrameworkCore;
 
 namespace Taller.App.Persistencia.AppRepositorios
@@ -13,10 +13,17 @@ namespace Taller.App.Persistencia.AppRepositorios
          protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             try{
+                if(!optionsBuilder.IsConfigured)
+                {
                 optionsBuilder.UseSqlServer("Server=tcp:server-tallertic-4.database.windows.net,1433;Initial Catalog=bd_tallertic;Persist Security Info=False;User ID=admintic;Password=Tallermintic4;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
                 }
-            catch{
-                
+
+                }
+            catch(System.Exception)
+            {
+                    Console.WriteLine("ocurrio un error en Onconfiguring");
+                    throw;
+
                 }
         }
     }
