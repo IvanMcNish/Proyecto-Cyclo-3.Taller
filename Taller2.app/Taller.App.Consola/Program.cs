4804e33f97fc33f922a555465ef58c87ad4edbd5
IvanMcNish <cesar_mcnish@hotmail.com>
2022-09-15 20:25:49 -0500

    codigo adelantado hasta el lunes

--- Taller2.app/Taller.App.Consola/Program.cs
+++ Taller2.app/Taller.App.Consola/Program.cs
@@ -1,16 +1,30 @@
 ﻿
 using System;
-
-
+using Taller.App.Persistencia;
+using Taller.App.Dominio;
 namespace Taller.App.Consola{
 
     class Program{
-
+        private static RepositorioMecanico repoMecanico = new RepositorioMecanico(
+            new Persistencia.ContextDb()
+        );
 
         static void Main (string[] args)
         {
+            Console.WriteLine("");
 
+        }
 
+        static void AgregarMecanico(){
+            var mecanico = new Mecanico {
+                id="23",
+                nombre="juan",
+                fechaNacimiento="",
+                nivelEstudio="bachiller",
+                telefono="123",
+                contrasenia="123",
+                rol="jefeoperaciones",
+            };
         }
     }
 
